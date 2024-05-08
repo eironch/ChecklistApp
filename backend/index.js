63b84b486902a2ccbc03bab0620c60a4517fc331
ficuno <eldritchgallows@gmail.com>
2024-05-08 17:53:10 +0800

    change db connection

--- backend/index.js
+++ backend/index.js
@@ -4,23 +4,15 @@ import cors from "cors"
 
 const app = express();
 
-// const pool = mysql.createPool({
-//     connectionLimit: 10,
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME
-// });
-
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "oi5.h.filess.io",
-    user: "checklist_anythingdo",
-    password: "08d63b649dd8d73e411bfc7d848ba1f4f540e72b",
-    database: "checklist_anythingdo",
-    port: "3307"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
-    
+
 app.use(express.json())
 app.use(cors(
     // {origin: 'https://checklist-app-client.vercel.app',
