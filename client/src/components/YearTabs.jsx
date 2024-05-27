0defaffa09c601fd6bdbd260b7c55def20c8d206
ficuno <eldritchgallows@gmail.com>
2024-05-28 00:27:26 +0800

    add mid year category

--- client/src/components/YearTabs.jsx
+++ client/src/components/YearTabs.jsx
@@ -20,6 +20,9 @@ function YearTabs(props) {
                 <button className={`${props.query.year === "3" ? "bg-gray-500" : "bg-gray-600 "} px-4 py-2 relative z-10 border text-gray-300 border-gray-400 hover:bg-gray-400 hover:text-gray-100`} name="3" onClick={ handleYearChange }>
                     Junior
                 </button>
+                <button className={`${props.query.year === "3" ? "bg-gray-500" : "bg-gray-600 "} px-4 py-2 relative z-10 border text-gray-300 border-gray-400 hover:bg-gray-400 hover:text-gray-100`} name="3.5" onClick={ handleYearChange }>
+                    Mid Year
+                </button>
                 <button className={`${props.query.year === "4" ? "bg-gray-500" : "bg-gray-600 "} px-4 py-2 relative z-10 rounded-r-2xl border text-gray-300 border-gray-400 hover:bg-gray-400 hover:text-gray-100`} name="4" onClick={ handleYearChange }>
                     Senior
                 </button>
