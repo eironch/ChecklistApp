1d2233bfe38b6d79ec0aa4eb6c90efdf73f144cd
ficuno <eldritchgallows@gmail.com>
2024-12-27 01:37:03 +0800

    chore: hide infos for privacy

--- client/src/components/Info.jsx
+++ client/src/components/Info.jsx
@@ -13,37 +13,37 @@ function Info(props) {
                     Full Name
                 </div>
                 <div className="px-5 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 border-gray-400 overflow-hidden">
-                    {info?.student_name || 'N/A'}
+                    {/* {info?.student_name || 'N/A'} */}
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 font-bold border-gray-400 overflow-hidden">
                     Student Num
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 border-gray-400 overflow-hidden">
-                    {info?.student_num || 'N/A'}
+                    {/* {info?.student_num || 'N/A'} */}
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 font-bold border-gray-400 overflow-hidden">
                     Address
                 </div>
                 <div className="px-4 py-2 w-2/12 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 border-gray-400 overflow-hidden">
-                    {info?.student_address || 'N/A'}
+                    {/* {info?.student_address || 'N/A'} */}
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 font-bold border-gray-400 overflow-hidden">
                     Admission Date
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 border-gray-400 overflow-hidden">
-                    {info?.admission_date.split("T")[0] || 'N/A'}
+                    {/* {info?.admission_date.split("T")[0] || 'N/A'} */}
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 font-bold border-gray-400 overflow-hidden">
                     Contact Num
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 border-gray-400 overflow-hidden">
-                    {info?.contact_num || 'N/A'}
+                    {/* {info?.contact_num || 'N/A'} */}
                 </div>
                 <div className="px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 border text-gray-300 font-bold border-gray-400 overflow-hidden">
                     Adviser Name
                 </div>
                 <div className={`${info ? "w-1/12 " :  "w-3/12"} px-4 py-2 text-nowrap text-center bg-gray-500 relative z-10 rounded-r-2xl border text-gray-300 border-gray-400 overflow-hidden`}>
-                    {info?.adviser_name || 'N/A'}
+                    {/* {info?.adviser_name || 'N/A'} */}
                 </div>
             </div>
         </div>
